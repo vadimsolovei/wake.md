fc69969f222748a3c90e8b1b335451fd184bd000
Vadim Solovei <vadims.olovei90@gmail.com>
2021-04-30 22:14:22 +0300

    added promo block, removed grid first item, changed footer bg color and removed uneccesary footer styles

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,10 +1,10 @@
 // Smooth scroll to element
-const gridElement = document.getElementById("grid");
+const promoElement = document.getElementById("promo");
 const scrollButton = document.querySelector(".hero__chevron");
 
 function clickHandler(e) {
   e.preventDefault();
-  const offsetTop = gridElement.offsetTop - 80;
+  const offsetTop = promoElement.offsetTop - 80;
 
   scroll({
     top: offsetTop,
@@ -20,7 +20,7 @@ const checkpoint = 600;
 const opacityOnScroll = () => {
   const currentScroll = window.scrollY;
   if (currentScroll <= checkpoint) {
-    opacity = 0.9 - currentScroll / checkpoint + 0.1;
+    opacity = 1 - currentScroll / checkpoint + 0.1;
   } else {
     opacity = 0.1;
   }
