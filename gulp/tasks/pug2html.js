7e1b8bb241bab67cbe6759fec3e8901c29d8db1b
Vladislav Cartovenco <slavdis@Vladislavs-MacBook-Air.local>
2022-08-07 11:06:19 +0300

    temp hotfix to prevent build errors

--- gulp/tasks/pug2html.js
+++ gulp/tasks/pug2html.js
@@ -2,7 +2,6 @@ const gulp = require('gulp')
 const plumber = require('gulp-plumber')
 const pug = require('gulp-pug')
 const pugLinter = require('gulp-pug-linter')
-const htmlValidator = require('gulp-w3c-html-validator')
 const bemValidator = require('gulp-html-bem-validator')
 const config = require('../config')
 
@@ -11,5 +10,6 @@ module.exports = function pug2html() {
     .pipe(plumber())
     .pipe(pugLinter({ reporter: 'default' }))
     .pipe(pug({ pretty: config.pug2html.beautifyHtml }))
+    .pipe(bemValidator())
     .pipe(gulp.dest('build'))
 }
