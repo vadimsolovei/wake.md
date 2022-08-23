da436d88b7f4288d3a3870d163ba0fa37ffc5932
Vladislav Cartovenco <slavdis@gmail.com>
2022-08-23 22:31:49 +0300

    basic popup functionality

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,4 @@ import "./slider";
 import "./scroll";
 import "./popup-close";
 import "./viewportCount";
+import "./popups";
---
+++ src/js/popups.js
@@ -0,0 +1,18 @@
+const bodyEl = document.querySelector("body");
+const closeButtonEl = document.querySelector(".js-popup_close");
+const popup = document.querySelector(".js-popup");
+const popupOpener = document.querySelector(".js-popup_opener");
+
+const onCloseButtonClick = () => {
+  popup.classList.remove("active");
+  bodyEl.classList.remove("popup-active");
+};
+
+const onOpenPopupClick = (e) => {
+  e.preventDefault();
+  popup.classList.add("active");
+  bodyEl.classList.add("popup-active");
+};
+
+closeButtonEl.addEventListener("click", onCloseButtonClick);
+popupOpener.addEventListener("click", onOpenPopupClick);
